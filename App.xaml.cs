0de039cf26c69a5ce4a72ea7651ece091ef14a7e
yuxuan <xuanyuong@gmail.com>
2024-12-12 18:17:36 +0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Yx_Simple_Mp3_Player
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
